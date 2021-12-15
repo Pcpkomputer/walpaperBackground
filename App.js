880646b3d87f99850f991050890ed784295c08e5
pcpkomputer <terrificsubs@gmail.com>
2021-12-15 11:27:22 +0700

    feat: extended estylesheet

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+
+const entireScreenWidth = Dimensions.get('window').width;
+EStyleSheet.build({$rem: entireScreenWidth / 380});
 
 export default function App() {
   return (
