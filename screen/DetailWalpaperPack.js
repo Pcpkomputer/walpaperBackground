47fa62b079abdc7b51c75670fcb719e0415446a3
pcpkomputer <terrificsubs@gmail.com>
2022-01-07 11:12:20 +0700

    feat: ...

--- screen/DetailWalpaperPack.js
+++ screen/DetailWalpaperPack.js
@@ -27,7 +27,7 @@ export default function DetailWalpaperPack(props) {
 
     let showInterstitialAds = async ()=>{
         await AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712'); 
-        await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true});
+        await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: false});
         await AdMobInterstitial.showAdAsync();
       }
     
